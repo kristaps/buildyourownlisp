85f48964d17de18e567a2c214a52fc48091bb5b6
Kristaps Rāts <kristaps.rats@gmail.com>
2014-04-19 13:59:24 +0300

    Exercises from chapters 2, 4 and 6.

---
+++ ch4_interactive_prompt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+
+int main(int argc, char** argv) {
+	puts("Krispy version 0.0.0.0.1");
+	puts("Press Ctrl+C to Exit\n");
+
+	while (1) {
+		char* input = readline("krispy> ");
+		add_history(input);
+		printf("No, you're a %s\n", input);
+		free(input);
+	}
+
+	return 0;
+}
